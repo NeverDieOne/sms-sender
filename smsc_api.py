525a371d74c230eee11e0c2e078c04ba62b3c7ac
neverdieone <altiore@mail.ru>
2021-11-04 22:34:06 +0300

    Add sms status check

--- smsc_api.py
+++ smsc_api.py
@@ -1,11 +1,38 @@
-from typing import DefaultDict
 import asks
 import asyncclick as click
 from environs import Env
+import trio
 
 
 env = Env()
 env.read_env()
+
+
+async def send_message(login, password, phone, message):
+    url = 'https://smsc.ru/sys/send.php'
+    params = {
+        'login': login,
+        'psw': password,
+        'phones': phone,
+        'mes': message,
+        'valid': 1,
+        'fmt': 3,
+    }
+    response = await asks.post(url, params=params)
+    return response.json()
+
+
+async def get_status(login, password, phone, message_id):
+    url = 'https://smsc.ru/sys/status.php'
+    params = {
+        'login': login,
+        'psw': password,
+        'phone': phone,
+        'id': message_id,
+        'fmt': 3
+    }
+    response = await asks.get(url, params=params)
+    return response.json()
 
 
 @click.command()
@@ -14,16 +41,10 @@ env.read_env()
 @click.option('-m', '--message', help='Текст сообщения', default=env.str('MESSAGE'))
 @click.option('-p', '--phone', help='Номер телефона', default=env.str('PHONE'))
 async def main(login, password, message, phone):
-    url = 'https://smsc.ru/sys/send.php'
-    params = {
-        'login': login,
-        'psw': password,
-        'phones': phone,
-        'mes': message,
-        'valid': 1
-    }
-    response = await asks.post(url, params=params)
-    print(response)
+    message = await send_message(login, password, phone, message)
+    await trio.sleep(1)
+    status = await get_status(login, password, phone, message['id'])
+    print(status)
 
 
 if __name__ == '__main__':
